80455eaef2f931e9d7add452c0052b5f09c5ee55
Weiwei <vivianqiu231@gmail.com>
2015-08-22 16:14:30 -0500

    changed

---
+++ crarand.m
@@ -0,0 +1,15 @@
+load('cra1_138.mat');
+a=1:138;
+b=Shuffle(a)';
+cra_rand=cell(138,5);
+for i=1:138
+    cra_rand{i,1}=b(i);
+    k=b(i);
+    cra_rand{i,2}=CRA2015S7{k,1};
+    cra_rand{i,3}=CRA2015S7{k,2};
+    cra_rand{i,4}=CRA2015S7{k,3};
+    cra_rand{i,5}=CRA2015S7{k,4};
+end
+
+
+%%%%fdjdslfjdksfjd   to git
